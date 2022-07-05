ef9fe0c41c40a5f239bfec1022e6e680fad0958b
Leon <leonvila98@gmail.com>
2022-07-05 20:50:56 -0300

    fix env variables

--- src/SupabaseClient.js
+++ src/SupabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-const SUPABASE_URL = process.env.SUPABASE_API_URL;
-const SUPABASE_TOKEN = process.env.SUPABASE_TOKEN;
+const API = process.env.REACT_APP_SUPABASE_API_URL;
+const TOKEN = process.env.REACT_APP_SUPABASE_TOKEN;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_TOKEN);
+export const supabase = createClient(API, TOKEN);
